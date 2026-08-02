6ec483185e7d967ef7691e662d98a3ba3bb27fb9
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-08-02 11:46:42 +0300

    init

---
+++ src/client/java/notenlish/buildersgauntlet/client/BuilderSGauntletClient.java
@@ -0,0 +1,10 @@
+package notenlish.buildersgauntlet.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BuilderSGauntletClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
